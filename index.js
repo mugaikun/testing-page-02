ce9eb34dd328ad400b9d2cbc629f5ebfcc100c8f
adan <adans777x@hotmail.com>
2022-06-06 23:17:01 -0400

    02-añadido mongoose

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const { create } = require("express-handlebars");
+require("dotenv").config(); //lee variables de entorno ".env"
+require("./database/db"); // lee base de datos
 
 const app = express();
 const port = 3000;
@@ -47,6 +49,7 @@ app.use("/auth", require("./routes/auth"));
 // });
 
 /*  */
-app.listen(port, () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
   console.log(`Example app listening on port ${port}!`);
 });
